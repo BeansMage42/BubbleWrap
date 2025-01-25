6241477d6bcdf0a9dd97bf78a0773eaab5ad90cf
ArmoredNT <zachary.obrien@ontariotechu.net>
2025-01-25 00:32:26 -0500

    Bubble Popping

---
+++ Assets/Scripts/BubblePop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblePop : MonoBehaviour
+{
+    [SerializeField] private bool play;
+    private Material mat;
+
+    private ParticleSystem ps;
+
+    private float popAmount;
+    private bool pop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        ps = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play)
+        {
+            play = false;
+            pop = true;
+            ps.Play();
+        }
+
+        if (pop)
+        {
+            if (popAmount < 1)
+            {
+                popAmount += Time.deltaTime * 3;
+                mat.SetFloat("_DissolveAmount", popAmount);
+            }
+            else
+            {
+                Destroy(GetComponent<MeshRenderer>());
+                pop = false;
+            }
+        }
+    }
+}
